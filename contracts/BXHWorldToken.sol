60c4cd64dd5de97bbfab25e8736c04c71c996122
emanherawy <eman.herawy@gmail.com>
2022-05-27 14:21:55 +0200

    contract optimized, add events ad make contract data configurable

--- contracts/BXHWorldToken.sol
+++ contracts/BXHWorldToken.sol
@@ -12,18 +12,33 @@ import './ERC20PresetMinterPauser.sol';
  */
 contract BXHWorldToken is ERC20Permit, ERC20PresetMinterPauser, ERC20Capped{
 
-    uint256 _holdersReflectionFees;
-    uint256 _bXHPaybackLiquidityFees;
-    bool _burnActivated;
-    address _holdersReflectionWallet;
-    address _bXHPaybackLiquidityWallet;
-       constructor(
+    uint256 private _holdersReflectionFees;
+    uint256 private _bXHPaybackLiquidityFees;
+    bool private _burnActivated;
+    address private _holdersReflectionWallet;
+    address private _bXHPaybackLiquidityWallet;
+    event HolderFeesUpdated(uint256 newFees,address from );
+    event HolderWalletUpdated(address newWallet,address from );
+    event BXHPaybackLiquidityFeesUpdated(uint256 newFees,address from );
+    event BXHPaybackLiquidityWalletUpdated(address newWallet,address from );
+        constructor(
         string memory _name,
         string memory _symbol,
         uint256 _cap,
         address _owner
     )ERC20Permit(_name) ERC20PresetMinterPauser(_name,_symbol,_owner) ERC20Capped(_cap) {
     }
+// private functions 
+    function _getShares(uint256 _amount) view  private returns (uint256 taxShare,uint256 holderShare,uint256 remaining) {
+        taxShare = _amount*(_bXHPaybackLiquidityFees/100);
+        holderShare = _amount*(_holdersReflectionFees/100);
+        remaining=_amount -(taxShare+holderShare);
+    }
+
+
+
+
+    // internals 
         function _mint(address account, uint256 amount) internal virtual override (ERC20,ERC20Capped)  {
         super._mint(account, amount);
     }
@@ -35,8 +50,64 @@ contract BXHWorldToken is ERC20Permit, ERC20PresetMinterPauser, ERC20Capped{
         super._beforeTokenTransfer(from, to, amount);
 
      }
+// externals 
+    function activateBurn() external {
+        require(hasRole(BURNER_ROLE, _msgSender()), "ElboxahToken: must have BURNER_ROLE");
+        _burnActivated =!_burnActivated;
+    }
+    function updateHolderFees(uint256 fee) external {
+        // TODO : ask about the mini ams max 
+                require(fee>0,"fees percentage must be more than zero");
+                require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "ElboxahToken: must have Admin_ROLE");
+                _holdersReflectionFees=fee;
+                // emit event 
+                emit HolderFeesUpdated(fee,  _msgSender());
+   
+    }
 
-    /**
+  function updateBXHPaybackLiquidityFees(uint256 fee) external {
+        // TODO : ask about the mini ams max 
+                require(fee>0,"fees percentage must be more than zero");
+                require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "ElboxahToken: must have Admin_ROLE");
+                _bXHPaybackLiquidityFees=fee;
+                // emit event 
+                emit BXHPaybackLiquidityFeesUpdated(fee,  _msgSender());
+   
+    }
+    function updateBXHPaybackLiquidityWallet(address newWallet) external {
+                require(newWallet!= address(0),"Zero address is not allowed ");
+                require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "ElboxahToken: must have Admin_ROLE");
+                _bXHPaybackLiquidityWallet=newWallet;
+                // emit event 
+                emit BXHPaybackLiquidityWalletUpdated(newWallet,  _msgSender());
+   
+    }
+    function updateHolderWallet(address newWallet) external {
+                require(newWallet!= address(0),"Zero address is not allowed ");
+                require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "ElboxahToken: must have Admin_ROLE");
+                _holdersReflectionWallet=newWallet;
+                // emit event 
+                emit HolderWalletUpdated(newWallet,  _msgSender());
+   
+    }
+
+function burnActivated() view external returns (bool ) {
+    return _burnActivated;
+}
+    function holderWallet() view external returns (address) {
+        return _holdersReflectionWallet;
+    }
+    function bXHPaybackLiquidityWallet() view external returns (address) {
+        return _bXHPaybackLiquidityWallet;
+    }
+    function holderFees() view external returns (uint256) {
+        return _holdersReflectionFees;
+    }
+    function bXHPaybackLiquidityFees() view external returns (uint256) {
+        return _bXHPaybackLiquidityFees;
+    }
+    // public
+       /**
      * @dev See {IERC20-transfer}.
      *
      * Requirements:
@@ -77,13 +148,5 @@ contract BXHWorldToken is ERC20Permit, ERC20PresetMinterPauser, ERC20Capped{
 
         super.burn(amount);
     }
-    function _getShares(uint256 _amount) view  private returns (uint256 taxShare,uint256 holderShare,uint256 remaining) {
-        taxShare = _amount*(_bXHPaybackLiquidityFees/100);
-        holderShare = _amount*(_holdersReflectionFees/100);
-        remaining=_amount -(taxShare+holderShare);
-    }
-    function activateBurn() external {
-        require(hasRole(BURNER_ROLE, _msgSender()), "ElboxahToken: must have BURNER_ROLE");
-        _burnActivated =!_burnActivated;
-    }
+  
 }
